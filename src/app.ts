435d2d843ae5550974ceacf7e0f1c40a2270aa98
Carlos Solis <mrsoliscalderon@gmail.com>
2023-11-25 15:00:02 +0100

    add huskies files, public folder and db.connect

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+// // Import express from 'express';
+// // import cors from 'cors';
+// // import morgan from 'morgan';
+// // import { tasksRouter } from './routers/tasks.router.js';
+// // import { notesRouter } from './routers/notes.router.js';
+// // import createDebug from 'debug';
+
+// const debug = createDebug('W7E:app');
+
+// export const app = express();
+// debug('Starting');
+
+// app.use(cors());
+// app.use(morgan('dev'));
+
+// app.use(express.json());
+// app.use(express.static('public'));
+
+// app.use('/tasks', tasksRouter);
+// app.use('/notes', notesRouter);
+// app.use('/users', usersRouter);
+
+// app.use(errorMiddleware);
