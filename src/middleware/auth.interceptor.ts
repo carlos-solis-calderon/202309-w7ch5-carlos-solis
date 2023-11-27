424b5599d311d7c2918b6cee01d2c7548f1b8ee8
Carlos Solis <mrsoliscalderon@gmail.com>
2023-11-27 09:40:30 +0100

    add index and auth method

--- src/middleware/auth.interceptor.ts
+++ src/middleware/auth.interceptor.ts
@@ -11,7 +11,7 @@ export class AuthInterceptor {
     debug('Instantiated');
   }
 
-  authorization(req: Request, _res: Response, next: NextFunction) {
+  authorization(req: Request, res: Response, next: NextFunction) {
     try {
       const tokenHeader = req.get('Authorization');
       if (!tokenHeader?.startsWith('Bearer'))
